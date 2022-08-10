ac17554bab0d6c8b26ee3120c570ece09df0f024
Bruno Peres <bruno_16515@hotmail.com>
2022-08-10 09:01:50 -0300

    Rotas adicionadas

--- server/server.js
+++ server/server.js
@@ -1,9 +1,42 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Request made");
+  console.log("Request: ", req.url, req.method);
+
+  // Set Headers Content-Type
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  // Send an HTML file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //res.write(data);
+      
+      res.end(data);
+    }
+  });
 });
 
-server.listen(3000, 'localhost', () => {
+server.listen(3000, "localhost", () => {
   console.log("Listening on port 3000");
 });
